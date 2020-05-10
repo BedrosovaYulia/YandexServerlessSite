7c6585cc36cf6246f9ea465c19e09314f41bc059
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-05-10 16:46:26 +0300

    creation a results list page

--- Functions/demo1form1/lambda_function.py
+++ Functions/demo1form1/lambda_function.py
@@ -8,13 +8,14 @@ def lambda_handler(event,context):
     s3 = session.client(
         service_name='s3',
         endpoint_url='https://storage.yandexcloud.net',
-        aws_access_key_id = '*****************',
-        aws_secret_access_key = '**************************************',
+        aws_access_key_id = os.environ['key_id'],
+        aws_secret_access_key = os.environ['access_key'],
     )
 
-    
-    #save data
-    s3.put_object(Bucket='ybwsstorage1', Key=event['queryStringParameters']['email'], Body=str(event['queryStringParameters']))
+    # Загрузить объекты в бакет
+
+    ## Из строки
+    s3.put_object(Bucket=os.environ['bucket'], Key=event['queryStringParameters']['email'], Body=str(event['queryStringParameters']))
 
 
     return{
