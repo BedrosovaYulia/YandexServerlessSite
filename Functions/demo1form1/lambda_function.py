48d721e399899fa0ecf26336227d2b0a13aeada4
BedrosovaYulia <BedrosovaYulia@gmail.com>
2020-05-11 12:14:57 +0300

    bug fixed

--- Functions/demo1form1/lambda_function.py
+++ Functions/demo1form1/lambda_function.py
@@ -2,23 +2,23 @@ import os
 import json
 import boto3
 
-def lambda_handler(event,context):
+def lambda_handler(event, context):
 
     session = boto3.session.Session()
     s3 = session.client(
         service_name='s3',
         endpoint_url='https://storage.yandexcloud.net',
-        aws_access_key_id = os.environ['key_id'],
-        aws_secret_access_key = os.environ['access_key'],
+        aws_access_key_id=os.environ['key_id'],
+        aws_secret_access_key=os.environ['access_key'],
     )
 
     # Загрузить объекты в бакет
 
     ## Из строки
-    s3.put_object(Bucket=os.environ['bucket'], Key=event['queryStringParameters']['email'], Body=str(json.dumps(event['queryStringParameters'])))
-
+    s3.put_object(Bucket=os.environ['bucket'], Key=event['queryStringParameters']['email'], Body=str(
+        json.dumps(event['queryStringParameters'])))
 
     return{
-        'statusCode':200,
-        'body':json.dumps('Hello from Yandex Function')
+        'statusCode': 200,
+        'body': json.dumps('Hello from Yandex Function')
     }
--- Functions/demo1list/lambda_function.py
+++ Functions/demo1list/lambda_function.py
@@ -2,23 +2,25 @@ import os
 import json
 import boto3
 
-def lambda_handler(event,context):
+def lambda_handler(event, context):
 
     session = boto3.session.Session()
     s3 = session.client(
         service_name='s3',
         endpoint_url='https://storage.yandexcloud.net',
-        aws_access_key_id = os.environ['key_id'],
-        aws_secret_access_key = os.environ['access_key'],
+        aws_access_key_id=os.environ['key_id'],
+        aws_secret_access_key=os.environ['access_key'],
     )
 
     # Получаем данные
-    messagelist=dict()
+    messagelist = dict()
     for key in s3.list_objects(Bucket=os.environ['bucket'])['Contents']:
-        get_object_response = s3.get_object(Bucket=os.environ['bucket'],Key=key['Key'])
-        messagelist[key['Key']]=json.loads(get_object_response['Body'].read().decode())
+        get_object_response = s3.get_object(
+            Bucket=os.environ['bucket'], Key=key['Key'])
+        messagelist[key['Key']] = json.loads(
+            get_object_response['Body'].read().decode())
 
     return{
-        'statusCode':200,
-        'body':json.dumps(messagelist)
+        'statusCode': 200,
+        'body': json.dumps(messagelist)
     }
